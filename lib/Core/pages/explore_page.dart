f004d05adc2c737b9d1ee94ed3f56867a6fd990b
kirmada150905 <174258997+kirmada150905@users.noreply.github.com>
2025-01-15 16:05:09 +0530

    provider category slugs

--- lib/Core/pages/explore_page.dart
+++ lib/Core/pages/explore_page.dart
@@ -1,3 +1,4 @@
+import 'package:bitshop/Core/product_listing_widgets.dart';
 import 'package:bitshop/helpers/product_providers.dart';
 import 'package:bitshop/styles/colors.dart';
 import 'package:carousel_slider/carousel_slider.dart';
@@ -20,25 +21,12 @@ class _ExplorePageState extends ConsumerState<ExplorePage>
   @override
   Widget build(BuildContext context) {
     super.build(context); // Required for `AutomaticKeepAliveClientMixin`
-    final categorySlugs = ref.watch(categorySlugsProvider);
+
+    final categorySlugsAsync = ref.watch(categorySlugsProvider);
 
     return SafeArea(
-      child: categorySlugs.when(
-        data: (slugs) {
-          if (slugs.isEmpty) {
-            return const Center(child: Text("No categories available."));
-          }
-
-          final validSlugs = slugs.where((slug) {
-            final productsAsyncValue =
-                ref.read(productsByCategoryProvider(slug));
-            return productsAsyncValue.when(
-              data: (products) => products != null && products.isNotEmpty,
-              error: (error, stackTrace) => false,
-              loading: () => false,
-            );
-          }).toList();
-
+      child: categorySlugsAsync.when(
+        data: (validSlugs) {
           if (validSlugs.isEmpty) {
             return const Center(
               child: Text(
@@ -52,99 +40,51 @@ class _ExplorePageState extends ConsumerState<ExplorePage>
             itemCount: validSlugs.length,
             itemBuilder: (context, index, realIndex) {
               final slug = validSlugs[index];
-              return GestureDetector(
-                onTap: () {
-                  context.push("/productByCategory_screen", extra: slug);
+              final asyncValue = ref.watch(productsByCategoryProvider(slug));
+              return asyncValue.when(
+                data: (products) {
+                  return GestureDetector(
+                    onTap: () {
+                      context.push("/productByCategory_screen", extra: slug);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 16.0),
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: lightBlue,
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            blurRadius: 8.0,
+                            spreadRadius: 2.0,
+                          ),
+                        ],
+                      ),
+                      child: ListView(
+                        shrinkWrap: true,
+                        children: [
+                          Text(
+                            slug.toUpperCase(),
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 2,
+                              color: cream,
+                            ),
+                          ),
+                          ProductGridBuilder(products: products!),
+                        ],
+                      ),
+                    ),
+                  );
                 },
-                child: Container(
-                  margin: const EdgeInsets.symmetric(
-                      horizontal: 8.0, vertical: 16.0),
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    color: lightBlue,
-                    borderRadius: BorderRadius.circular(16.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        blurRadius: 8.0,
-                        spreadRadius: 2.0,
-                      ),
-                    ],
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        slug.toUpperCase(),
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2,
-                          color: cream,
-                        ),
-                      ),
-                      const SizedBox(height: 16.0),
-                      Expanded(
-                        child: Consumer(
-                          builder: (context, ref, child) {
-                            final productsAsyncValue =
-                                ref.watch(productsByCategoryProvider(slug));
-
-                            return productsAsyncValue.when(
-                              data: (products) {
-                                return GridView.builder(
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 8),
-                                  shrinkWrap: true,
-                                  physics: const NeverScrollableScrollPhysics(),
-                                  gridDelegate:
-                                      const SliverGridDelegateWithFixedCrossAxisCount(
-                                    crossAxisCount: 2,
-                                    crossAxisSpacing: 10.0,
-                                    mainAxisSpacing: 10.0,
-                                    childAspectRatio: 1.0,
-                                  ),
-                                  itemCount: products?.length,
-                                  itemBuilder: (context, index) {
-                                    final product = products?[index];
-                                    return Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color:
-                                                Colors.black.withOpacity(0.1),
-                                            blurRadius: 8.0,
-                                            spreadRadius: 2.0,
-                                          ),
-                                        ],
-                                      ),
-                                      child: ClipRRect(
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                        child: Image.network(
-                                          product?.thumbnail ?? "",
-                                          fit: BoxFit.cover,
-                                          errorBuilder:
-                                              (context, error, stackTrace) =>
-                                                  const Icon(Icons.image),
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                );
-                              },
-                              error: (error, stackTrace) =>
-                                  const SizedBox.shrink(),
-                              loading: () => const SizedBox.shrink(),
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                error: (error, stackTrace) {
+                  return Center(child: Text("Error loading category $slug."));
+                },
+                loading: () => const Center(
+                  child: CircularProgressIndicator(),
                 ),
               );
             },
@@ -156,7 +96,8 @@ class _ExplorePageState extends ConsumerState<ExplorePage>
             ),
           );
         },
-        error: (error, stackTrace) => const SizedBox.shrink(),
+        error: (error, stackTrace) =>
+            const Center(child: Text("Error loading categories.")),
         loading: () => const Center(
           child: CircularProgressIndicator(),
         ),
